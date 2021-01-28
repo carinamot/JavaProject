5fbd06b560ae4a02264421371435a06433435ee8
ONtegra <ONtegra@DESKTOP-5JIGQFB>
2021-01-28 13:46:17 +0200

    ViewController

---
+++ Ex/src/Controller/ViewController.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import ex.Model.Board;
+
+public class ViewController {
+
+	CommandController commandController=new CommandController();
+	
+	public void displayBoard(String board) {
+		String[] arr=commandController.decomposeCommand(board);
+		int k=0;
+		for(int i=0; i< Board.numRows; i++) {
+			for(int j=0; j< Board.numCols; j++)
+			{
+				k++;
+				System.out.print(arr[k]+" ");
+			}
+			System.out.println("\n");
+		}
+	}
+	
+	public void displayList(String usernames) {
+		String [] arr= commandController.decomposeCommand(usernames);
+		for(int i=1; i< arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
