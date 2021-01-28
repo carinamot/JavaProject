ab91825d25b2f58984b60c460d12353c3e2d3aae
ONtegra <ONtegra@DESKTOP-5JIGQFB>
2021-01-28 13:58:16 +0200

    ClientThread

--- Ex/src/ex/ClientThread.java
+++ Ex/src/ex/ClientThread.java
@@ -4,9 +4,9 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-
-import Controller.ClientMessageController;
+import Controller.*;
 import ex.Model.Player;
+import ex.Model.Command.CommandType;
 import ex.Service.CommunicationService;
 import ex.Service.ConsoleService;
 
@@ -15,11 +15,15 @@ public class ClientThread implements Runnable {
 	private final Player player;
 	private final ClientMessageController messageController;
 	private final ConsoleService console;
+	private final CommandController commandController;
+	private final ViewController viewController;
 	
 	public ClientThread(Socket socket, CommunicationService pipe) {
 		player = new Player(socket);
 		messageController = new ClientMessageController(pipe);
 		console = new ConsoleService();
+		commandController= new CommandController();
+		viewController = new ViewController();
 	}
 
 	@Override
@@ -38,6 +42,15 @@ public class ClientThread implements Runnable {
 	
 	private void onCommandGot(String command) throws IOException {
 		console.write(command);
+		String[] parts = commandController.decomposeCommand(command);
+		CommandType commandType = commandController.verifyCommand(parts[0]);
+		
+		switch (commandType) {
+			case LIST:
+				viewController.displayList(command);
+				break;
+		}
+
 	}
 
 }
